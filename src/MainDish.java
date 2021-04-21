d6beef8a03385cddb87dc0e05bf1b5ec8c2cfcf7
Vitali P <sharko8@gmail.com>
2021-04-21 05:26:58 +0300

    First commit

---
+++ src/MainDish.java
@@ -0,0 +1,31 @@
+package takeout;
+
+public class MainDish {
+  private String mainDishName;
+  private double mainDishPrice;
+
+  MainDish(String name, double price) {
+    this.mainDishName = name;
+    this.mainDishPrice = price;
+  }
+
+  MainDish() {
+
+  }
+
+  public String getMainDishName() {
+    return mainDishName;
+  }
+
+  public double getMainDishPrice() {
+    return mainDishPrice;
+  }
+
+  public void setMainDishName(String name) {
+    this.mainDishName = name;
+  }
+
+  public void setMainDishPrice(double price) {
+    this.mainDishPrice = price;
+  }
+}
